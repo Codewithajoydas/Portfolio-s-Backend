f8d445eec06c89d32627a28265e909270106d5ce
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2025-08-05 12:57:48 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { sendmail } from "./utility/mailSender.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
